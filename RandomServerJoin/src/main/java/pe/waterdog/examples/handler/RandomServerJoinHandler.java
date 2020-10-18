6b87e06138301bc9ec5e62c3312184cf6ad6807d
TobiasDev <tobiasdevde@gmail.com>
2020-10-18 15:48:00 +0200

    More example plugins

---
+++ RandomServerJoin/src/main/java/pe/waterdog/examples/handler/RandomServerJoinHandler.java
@@ -0,0 +1,20 @@
+package pe.waterdog.examples.handler;
+
+import com.google.common.collect.Iterables;
+import pe.waterdog.network.ServerInfo;
+import pe.waterdog.player.ProxiedPlayer;
+import pe.waterdog.utils.types.IJoinHandler;
+
+import java.util.Collection;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomServerJoinHandler implements IJoinHandler {
+    @Override
+    public ServerInfo determineServer(ProxiedPlayer proxiedPlayer) {
+        Collection<ServerInfo> servers = proxiedPlayer.getProxy().getServers();
+        if (servers.size() > 0) {
+            return Iterables.get(servers, ThreadLocalRandom.current().nextInt(servers.size()));
+        }
+        return null;
+    }
+}
---
+++ UtilityCommands/src/main/java/pe/waterdog/examples/UtilityCommands.java
@@ -0,0 +1,16 @@
+package pe.waterdog.examples;
+
+import pe.waterdog.examples.commands.GotoCommand;
+import pe.waterdog.examples.commands.KickCommand;
+import pe.waterdog.examples.commands.ServerPingCommand;
+import pe.waterdog.plugin.Plugin;
+
+public class UtilityCommands extends Plugin {
+
+    @Override
+    public void onEnable() {
+        getProxy().getCommandMap().registerCommand("goto", new GotoCommand());
+        getProxy().getCommandMap().registerCommand("kick", new KickCommand());
+        getProxy().getCommandMap().registerCommand("serverping", new ServerPingCommand());
+    }
+}
