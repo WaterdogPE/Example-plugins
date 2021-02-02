adbacde2ea1c3e732db378322fefa99e1b8a1453
Tobias G <tobiasdevde@gmail.com>
2021-02-02 14:51:45 +0100

    Implement the AddServer Command as requested by the Community

--- UtilityCommands/src/main/java/pe/waterdog/examples/UtilityCommands.java
+++ UtilityCommands/src/main/java/pe/waterdog/examples/UtilityCommands.java
@@ -1,5 +1,6 @@
 package pe.waterdog.examples;
 
+import pe.waterdog.examples.commands.AddServerCommand;
 import pe.waterdog.examples.commands.GotoCommand;
 import pe.waterdog.examples.commands.KickCommand;
 import pe.waterdog.examples.commands.ServerPingCommand;
@@ -12,5 +13,6 @@ public class UtilityCommands extends Plugin {
         getProxy().getCommandMap().registerCommand("goto", new GotoCommand());
         getProxy().getCommandMap().registerCommand("kick", new KickCommand());
         getProxy().getCommandMap().registerCommand("serverping", new ServerPingCommand());
+        getProxy().getCommandMap().registerCommand("addserver", new AddServerCommand());
     }
 }
---
+++ UtilityCommands/src/main/java/pe/waterdog/examples/commands/AddServerCommand.java
@@ -0,0 +1,30 @@
+package pe.waterdog.examples.commands;
+
+import pe.waterdog.ProxyServer;
+import pe.waterdog.command.Command;
+import pe.waterdog.command.CommandSender;
+import pe.waterdog.command.CommandSettings;
+import pe.waterdog.network.ServerInfo;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+
+public class AddServerCommand extends Command {
+
+    public AddServerCommand() {
+        super("addserver", CommandSettings.builder().setDescription("Add a Server to the server list").setUsageMessage("/addserver <Name> <IP> <Port>").build());
+    }
+
+    @Override
+    public boolean onExecute(CommandSender commandSender, String s, String[] strings) {
+        if (strings.length < 3) {
+            commandSender.sendMessage(this.getUsageMessage());
+            return false;
+        }
+        ServerInfo i = new ServerInfo(strings[0], new InetSocketAddress(strings[1], Integer.parseInt(strings[2])), null);
+        ProxyServer.getInstance().registerServerInfo(i);
+        commandSender.sendMessage("Server added successfully!");
+
+        return true;
+    }
+}
